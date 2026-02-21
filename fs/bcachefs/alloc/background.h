3ac5b3e1d962c6375af31984969270046aacc057
Jul Lang <jullanggit@proton.me>
2026-02-21 18:38:09 +0100

    bcachefs: shrink: use the same usage_remove path as dev_remove

--- fs/bcachefs/alloc/background.h
+++ fs/bcachefs/alloc/background.h
@@ -337,8 +337,7 @@ static inline const struct bch_backpointer *alloc_v4_backpointers_c(const struct
 	return (void *) ((u64 *) &a->v + BCH_ALLOC_V4_BACKPOINTERS_START(a));
 }
 
-int bch2_dev_truncate_alloc(struct bch_fs *c, struct bch_dev *ca, u64 cutoff);
-int bch2_dev_remove_alloc(struct bch_fs *, struct bch_dev *);
+int bch2_dev_remove_alloc(struct bch_fs *, struct bch_dev *, u64);
 
 void bch2_recalc_capacity(struct bch_fs *);
 u64 bch2_min_rw_member_capacity(struct bch_fs *);
